bcb19900b2abee1278945e15f2d140b077730fdb
Mitar <mitar.git@tnode.com>
2011-02-12 17:35:32 +0100

    Made menu items active when opened.

--- navadd/navadd.py
+++ navadd/navadd.py
@@ -1,4 +1,5 @@
 from trac.core import *
+from trac.web.api import IRequestFilter
 from trac.web.chrome import INavigationContributor
 from trac.util import Markup
 
@@ -6,7 +7,7 @@ from genshi.builder import tag
 
 class NavAdd(Component):
     """ Allows to add items to main and meta navigation bar"""
-    implements(INavigationContributor)
+    implements(INavigationContributor, IRequestFilter)
 
     # INavigationContributor methods
     def get_active_navigation_item(self, req):
@@ -32,3 +33,52 @@ class NavAdd(Component):
                 target = 'mainnav'
 
             yield (target, a, tag.a(title, href=url))
+
+    # IRequestFilter methods
+    def pre_process_request(self, req, handler):
+        add = self.env.config.getlist('navadd', 'add_items', [])
+
+        for a in add:
+            url = self.env.config.get('navadd', '%s.url' % a)
+            perm = self.env.config.get('navadd', '%s.perm' % a)
+            target = self.env.config.get('navadd', '%s.target' % a)
+            forusers = self.env.config.getlist('navadd', '%s.forusers' % a, [])
+            setactive = self.env.config.getbool('navadd', '%s.setactive' % a, True)
+
+            if perm and not req.perm.has_permission(perm):
+                continue
+
+            if forusers and req.authname not in forusers:
+                continue
+
+            if not req.environ['PATH_INFO'].startswith(url):
+                continue
+
+            if target not in ('mainnav', 'metanav'):
+                target = 'mainnav'
+
+            items = req.chrome.get('nav', {}).get(target, [])
+
+            if not items:
+                continue
+
+            for i in items:
+                if i.get('name', '') == a:
+                    # We first disable all items
+                    for j in items:
+                        j['active'] = False
+
+                    # We enable our item
+                    i['active'] = True
+
+                    break
+
+            break
+
+        return handler
+
+    def post_process_request(req, template, content_type):
+        return (template, content_type)
+
+    def post_process_request(req, template, data, content_type):
+        return (template, data, content_type)
